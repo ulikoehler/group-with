2cbb5e69ea36a9eafcdbe6e465aee7a566596df3
Uli Köhler <ulikoehler@online.de>
2014-03-27 19:09:40 +0100

    Fix first simple unit test

--- GroupByTest.hs
+++ GroupByTest.hs
@@ -1,40 +1,35 @@
-module GroupByTest (tests) where
-
-import Distribution.TestSuite
+import Test.Hspec
+import Test.QuickCheck
+import Control.Exception (evaluate)
 import Control.Monad
 import Control.GroupBy
 import Data.Map (Map)
+import Data.List (sort)
 import qualified Data.Map as Map
 
-multimapElemEq (k1,v1) (k2,v2) = (k1 == k2) && (v1 == v2)
 
--- Check
-multimapListEq :: (Eq a, Eq b) => [(a,[b])] -> [(a,[b])] -> Bool
+multimapElemEq (k1,v1) (k2,v2) = (k1 == k2) && (sort v1 == sort v2)
+
+-- Check if two multimap-representing lists are equal,
+-- disregarding value list order
+multimapListEq :: (Ord a, Ord b, Eq a, Eq b) => [(a,[b])] -> [(a,[b])] -> Bool
 multimapListEq xs ys =
     let zipped = zipWith multimapElemEq xs ys
     in and zipped
 
-multimapEq :: (Eq a, Eq b) => Map a [b] -> Map a [b] -> Bool
+multimapEq :: (Ord a, Ord b, Eq a, Eq b) => Map a [b] -> Map a [b] -> Bool
 multimapEq x y =
     multimapListEq (Map.toList x) (Map.toList y)
 
+multimapTupleEq (x,y) = multimapEq x y
 
--- TODO Use proper assertion library
-testGroupBy = do
-    let data1 = ["a","abc","ab","bc"]
-    let fn1 = take 1
-    let ref1 = Map.fromList [("a",["a","abc","ab"]),("b",["bc"])]
-    let result = groupBy fn1 data1
-    unlessM (result `multimapEq` ref1) $
-        Fail $ "groupBy test 1 failed: " ++ show result
+main :: IO ()
+main = hspec $ do
+  describe "groupBy" $ do
+    it "should group a simple value correctly" $
+        let data_ = ["a","abc","ab","bc"]
+            fn = take 1
+            ref = Map.fromList [("a",["a","abc","ab"]),("b",["bc"])]
+            result = groupBy fn data_
+        in (result, ref) `shouldSatisfy` multimapTupleEq
 
-tests :: IO [Test]
-tests = return [Test groupByTest]
-    where
-        groupByTest = TestInstance
-            { run = testGroupBy
-            , name = "groupBy"
-            , tags = []
-            , options = []
-            , setOption = \_ _ -> Right testGroupBy
-            }

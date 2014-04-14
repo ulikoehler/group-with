bbb290c02fc67d99206957a943672661d329eb31
Uli Köhler <ulikoehler@online.de>
2014-04-14 17:03:37 +0200

    Added groupWithA implementation

--- Control/GroupWith.hs
+++ Control/GroupWith.hs
@@ -1,4 +1,4 @@
-
+{-# LANGUAGE Safe #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Control.GroupWiths
@@ -7,16 +7,16 @@
 -- Maintainer  :  ukoehler@techoverflow.net
 -- Stability   :  provisional
 -- Portability :  portable
---
+-- 
 -- A collection of grouping utility functions.
 -- For a given function that assigns a key to objects,
 -- provides functions that group said objects into a multimap
 -- by said key.
---
+-- 
 -- This can be used similarly to the SQL GROUP BY statement.
---
+-- 
 -- Provides a more flexible approach to GHC.Exts.groupWith
---
+-- 
 -- > groupWith (take 1) ["a","ab","bc"] == Map.fromList [("a",["a","ab"]), ("b",["bc"])]
 --  
 -----------------------------------------------------------------------------
@@ -24,11 +24,16 @@ module Control.GroupWith(
         MultiMap,
         groupWith,
         groupWithMultiple,
-        groupWithUsing
+        groupWithUsing,
+        groupWithA
     ) where
 
 import Data.Map (Map)
 import qualified Data.Map as Map
+
+import Control.Arrow (first, second)
+import Control.Applicative (Applicative, (<$>), liftA2, pure)
+import Data.Traversable (sequenceA)
 
 type MultiMap a b = Map a [b]
 
@@ -65,3 +70,22 @@ groupWithUsing :: (Ord b) =>
           -> [a] -- ^ The list to be grouped
           -> Map b c -- ^ The resulting key --> transformed value map
 groupWithUsing t c f xs = Map.fromListWith c $ map (\v -> (f v, t v)) xs
+
+-- | Fuse the functor from a tuple
+fuseT2 :: Applicative f => (f a, f b) -> f (a,b)
+fuseT2 = uncurry $ liftA2 (,)
+
+-- | Like fuseT2, but only requires the first element to be boxed in the applicative
+fuseT2First :: Applicative f => (f a, b) -> f (a,b)
+fuseT2First = fuseT2 . second pure
+
+-- | Group values in a list by a key, generated
+--   by a given function.
+--   Applicative version of 'groupWith'. See 'groupWith' for documentation.
+groupWithA :: (Ord b, Applicative f) =>
+             (a -> f b) -- ^ The function used to map a list value to its key
+          -> [a] -- ^ The list to be grouped
+          -> f (MultiMap b a) -- ^ The resulting key --> value multimap
+groupWithA f xs =
+  let kvList = sequenceA $ map fuseT2First [(f x, [x]) | x <- xs]
+  in Map.fromListWith (++) <$> kvList
